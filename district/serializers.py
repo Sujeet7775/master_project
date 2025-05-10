dbe50330caf01e56ad1c82d3c670c564a704aebc
Sujeet7775 <sujeet.d@metaphi.in>
2025-05-10 15:26:34 +0530

    gp_master app

--- district/serializers.py
+++ district/serializers.py
@@ -1,21 +1,6 @@
-from rest_framework import serializers
 from .models import District
-
-class DistrictAdminSerializer(serializers.ModelSerializer):
+from core.serializers import BaseAuditSerializer  # ✅ Import from core
+class DistrictAdminSerializer(BaseAuditSerializer):
     class Meta:
         model = District
         fields = ['id', 'package', 'district_name', 'district_code', 'created_by', 'updated_by', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_by', 'updated_by', 'created_at', 'updated_at']
-
-    def create(self, validated_data):
-        # Automatically set the created_by and updated_by fields to the logged-in user
-        user = self.context['request'].user
-        validated_data['created_by'] = user
-        validated_data['updated_by'] = user
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        # Automatically update the updated_by field to the logged-in user
-        user = self.context['request'].user
-        validated_data['updated_by'] = user
-        return super().update(instance, validated_data)
--- gp_master/serializers.py
+++ gp_master/serializers.py
@@ -1,20 +1,8 @@
-from rest_framework import serializers
+
+from core.serializers import BaseAuditSerializer
 from .models import GPMaster
 
-class GPMasterSerializer(serializers.ModelSerializer):
+class GPMasterSerializer(BaseAuditSerializer):
     class Meta:
         model = GPMaster
         fields = '__all__'
-
-    def create(self, validated_data):
-        # Automatically set the created_by and updated_by fields to the logged-in user
-        user = self.context['request'].user
-        validated_data['created_by'] = user
-        validated_data['updated_by'] = user
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        # Automatically update the updated_by field to the logged-in user
-        user = self.context['request'].user
-        validated_data['updated_by'] = user
-        return super().update(instance, validated_data)
